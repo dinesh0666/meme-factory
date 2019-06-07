26be936b6fa66bd0409415ee2d046b505203f274
RaghulSK_Contus <raghul.k@contus.in>
2019-06-07 14:51:59 +0530

    #comment - worked to clear the image once the modal is closed and also worked to made some changes while uploading the image

--- src/UploadImage.js
+++ src/UploadImage.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-
-class UploadImage extends Component{   
-
-    render(){
-        return <>        
-         <form onSubmit={(e)=>this.props.onSubmit(e)}>
-                <p>Choose and Upload your own Image</p>
-                <input type="file" name="myImage" onChange= {(e)=>this.props.handleImageChange(e)} />
-                <button type="submit" className="uploadImage">Upload</button>
-            </form>
-                
-        </>
-
-    }
-
-
+class UploadImage extends Component {
+  render() {
+    return (
+      <>
+        <form onSubmit={e => this.props.onSubmit(e)}>
+          <p>Choose and Upload your own Image</p>
+          <input
+            type="file"
+            name="myImage"
+            onChange={e => this.props.handleImageChange(e)}
+          />
+          {this.props.file ? (
+            <button type="submit" className="uploadImage">
+              Upload
+            </button>
+          ) : (
+            ""
+          )}
+        </form>
+      </>
+    );
+  }
 }
 
 export default UploadImage;
