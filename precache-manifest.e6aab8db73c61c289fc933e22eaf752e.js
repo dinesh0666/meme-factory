5c714ecf4876cccc95b6d654296018e83fe12d80
d0d059e <dhineshkumar.ds@walmart.com>
2024-08-29 15:26:11 +0530

    Updates

---
+++ precache-manifest.e6aab8db73c61c289fc933e22eaf752e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389cdd5c10984c13d45514ca8f0debee",
+    "url": "/meme-factory/index.html"
+  },
+  {
+    "revision": "3ddaf36da6b970c7aeaf",
+    "url": "/meme-factory/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "624615ad226b78d0285c",
+    "url": "/meme-factory/static/css/main.cec2bd0a.chunk.css"
+  },
+  {
+    "revision": "3ddaf36da6b970c7aeaf",
+    "url": "/meme-factory/static/js/2.a04799d0.chunk.js"
+  },
+  {
+    "revision": "624615ad226b78d0285c",
+    "url": "/meme-factory/static/js/main.66a16cfe.chunk.js"
+  },
+  {
+    "revision": "eab513a626b776c57f90",
+    "url": "/meme-factory/static/js/runtime~main.54924acf.js"
+  },
+  {
+    "revision": "6cf5fc945fa852cfb8e613e984713d91",
+    "url": "/meme-factory/static/media/avengero.6cf5fc94.ttf"
+  },
+  {
+    "revision": "a2b8c7f02027b7c3c2c8080f0ae5b8aa",
+    "url": "/meme-factory/static/media/meme_factory.a2b8c7f0.jpg"
+  }
+]);
